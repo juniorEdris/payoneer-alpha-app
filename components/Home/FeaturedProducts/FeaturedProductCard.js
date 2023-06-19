ea804244c3b3d91dd620d6870485143aa96cc8b6
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2023-06-20 01:36:27 +0600

    updated product link

--- components/Home/FeaturedProducts/FeaturedProductCard.js
+++ components/Home/FeaturedProducts/FeaturedProductCard.js
@@ -4,31 +4,33 @@ import Link from "next/link";
 const ProductCard = ({ product }) => {
   return (
     <section className="grid gap-5">
-      <div className="featured-product-card">
-        <div className="">
-          <div className="h-[245px] w-full relative overflow-hidden rounded-[25px] border border-[#f5f5f5]">
-            <span className="uppercase absolute top-6 left-6 bg-blue-600 z-10 p-1 text-white text-xs font-semibold  rounded-[4px] shadow-sm">
-              Pre-order
-            </span>
-            <Image
-              className="object-cover"
-              src={product?.thumbnail}
-              fill={true}
-              priority={false}
-              alt={product?.title}
-            />
+      <Link href={`/`}>
+        <div className="featured-product-card">
+          <div className="">
+            <div className="h-[245px] w-full relative overflow-hidden rounded-[25px] border border-[#f5f5f5]">
+              <span className="uppercase absolute top-6 left-6 bg-blue-600 z-10 p-1 text-white text-xs font-semibold  rounded-[4px] shadow-sm">
+                Pre-order
+              </span>
+              <Image
+                className="object-cover"
+                src={product?.thumbnail}
+                fill={true}
+                priority={false}
+                alt={product?.title}
+              />
+            </div>
+          </div>
+          <div className="grid gap-3 p-4 justify-items-center">
+            <h3 className="text-2xl font-semibold text-[#3B3849]">
+              {product?.brand}
+            </h3>
+            <p className="text-[#3B3849] uppercase">{product?.category}</p>
+            <p className="text-[#0E4B80] text-lg font-semibold">
+              ${product?.price.toFixed(2)} USD
+            </p>
           </div>
         </div>
-        <div className="grid gap-3 p-4 justify-items-center">
-          <h3 className="text-2xl font-semibold text-[#3B3849]">
-            {product?.brand}
-          </h3>
-          <p className="text-[#3B3849] uppercase">{product?.category}</p>
-          <p className="text-[#0E4B80] text-lg font-semibold">
-            ${product?.price.toFixed(2)} USD
-          </p>
-        </div>
-      </div>
+      </Link>
       <div className="text-center">
         <Link
           href={`/`}
